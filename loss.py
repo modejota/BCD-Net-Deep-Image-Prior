891e501fda2beef226d01546d8bc1697a08454f5
Fernando Pérez <ferztk@gmail.com>
2022-09-23 12:29:52 +0200

    remove prints

--- loss.py
+++ loss.py
@@ -25,14 +25,14 @@ def loss_BCD(out_Cnet, out_Mnet_mean, out_Mnet_var, Y, sigma_s2, MR, patch_size)
     term_kl1 = (torch.norm(out_Mnet_mean - MR, dim=1) ** 2) / (2 * sigma_s2)
     term_kl2 = (3 / 2) * (out_Mnet_var / sigma_s2 - torch.log(out_Mnet_var / sigma_s2) - 1)
     loss_kl = torch.sum(term_kl1 + term_kl2)
-    print('loss kl:',loss_kl)
+    # print('loss kl:',loss_kl)
 
     Cflat = out_Cnet.view(-1, 2, patch_size * patch_size)
     Y_rec = torch.matmul(out_Mnet_mean, Cflat)
     term_mse1 = torch.sum(torch.norm(Y.view(-1, 3, patch_size * patch_size) - Y_rec, dim=1) ** 2)
     term_mse2 = torch.sum(torch.matmul(3 * out_Mnet_var[0], Cflat[0] ** 2))
     loss_mse = term_mse1 + term_mse2
-    print('loss mse:',loss_mse)
+    # print('loss mse:',loss_mse)
 
     loss = loss_mse + loss_kl
 
