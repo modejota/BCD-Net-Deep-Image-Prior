0bca88cfcc5250768c72a1cd5c5f14293a299e6c
Franblueee <cacoqueman@gmail.com>
2022-10-09 15:39:09 +0200

    actualiza

--- code/utils/datasets.py
+++ code/utils/datasets.py
@@ -61,7 +61,22 @@ class OD_Dataset(torch.utils.data.Dataset):
 
         return img, od_img, self.mR
 
-class CamelyonDataset(OD_Dataset):
+class CamelyonDataset(torch.utils.data.Dataset):
+
+    def __init__(self, data_path, centers, patch_size=224, n_samples=None):
+        super().__init__()
+        self.data_path = data_path
+        self.centers = centers
+        self.patch_size = patch_size
+        self.n_samples = n_samples
+        self.image_files = self.scan_files()
+        self.img_list = self.load_data()
+        self.len = len(self.image_files)
+        self.mR = torch.tensor([
+                    [0.6442, 0.0928],
+                    [0.7166, 0.9541],
+                    [0.2668, 0.2831]
+                    ])
 
     def scan_files(self):
         
@@ -93,8 +108,42 @@ class CamelyonDataset(OD_Dataset):
                 patches_ids += normal_patches_dict[c]
             
         return patches_ids
+    
+    def load_data(self):
+        img_list = []
+        #od_img_list = []
+        for file in self.image_files:
+            img = cv2.imread(file)
+            img = img[:,:,::-1] # Changes BGR to RGB
 
-class WSSBDatasetTest(OD_Dataset):
+            #if (self.patch_size < img.shape[0]) or (self.patch_size < img.shape[1]):
+            #    img = npimg_random_crop_patch(img, self.patch_size)
+            img_list.append(img)
+
+            #od_img = rgb2od_np(img) #Range [0, 5.54]
+            #od_img = normalize_to1(od_img, -np.log(1/256), 0) # Range [0, 1]           
+            #od_img_list.append(od_img)
+
+        return img_list
+    
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        img = self.img_list[idx]
+
+        if (self.patch_size < img.shape[0]) or (self.patch_size < img.shape[1]):
+            img = npimg_random_crop_patch(img, self.patch_size)
+
+        od_img = rgb2od_np(img) #Range [0, 5.54]
+        od_img = normalize_to1(od_img, -np.log(1/256), 0)
+        
+        od_img = TVTF.to_tensor(od_img.copy()).type(torch.float32)
+        img = TVTF.to_tensor(img.copy()).type(torch.float32)
+
+        return img, od_img, self.mR
+
+class WSSBDatasetTest(torch.utils.data.Dataset):
 
     def __init__(self, data_path, organ_list=['Lung', 'Breast', 'Colon']):
         self.data_path = data_path
@@ -107,9 +156,7 @@ class WSSBDatasetTest(OD_Dataset):
                     ])
         self.image_files, self.sv_files = self.scan_files()
         self.img_list, self.od_img_list, self.C_gt_list, self.M_gt_list = self.load_data()
-        self.len = len(self.image_files)
-        
-        
+        self.len = len(self.image_files)        
 
     def scan_files(self):
         patches_ids = []
@@ -139,6 +186,9 @@ class WSSBDatasetTest(OD_Dataset):
             #C_gt_rgb_list.append(C_gt_rgb)
         return img_list, od_img_list, C_gt_list, M_gt_list
     
+    def __len__(self):
+        return self.len
+
     def __getitem__(self, idx):
         img, od_img, mR = super().__getitem__(idx)
         C_gt = torch.from_numpy(self.C_gt_list[idx]).type(torch.float32)
