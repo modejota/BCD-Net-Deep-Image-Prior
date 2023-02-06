e74efb6cc1c13c26a03cfc3adc05493519a27499
Franblueee <cacoqueman@gmail.com>
2023-02-06 12:24:08 +0100

    icip

--- bash_scripts/run_train_test.sh
+++ bash_scripts/run_train_test.sh
@@ -5,7 +5,7 @@
 rm -f output/salida_*_train*
 rm -f output/salida_*_test*
 
-export CUDA_VISIBLE_DEVICES=0,3
+export CUDA_VISIBLE_DEVICES=0,1
 
 ################################################################################################# 
 
@@ -15,18 +15,18 @@ batch_size=32
 
 #################################################################################################
 
-python code/train.py --val_type=normal --epochs=1 --cnet_name=unet6sft --batch_size=$batch_size --num_workers=$num_workers --n_samples=10000 > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
-python code/test.py --cnet_name=unet6sft --batch_size=$batch_size --num_workers=$num_workers --n_samples=10000 > output/salida_${SERVER}_test_${BASHPID}.txt 2>&1
+#python code/train.py --val_type=normal --epochs=1 --cnet_name=unet6sft --batch_size=$batch_size --num_workers=$num_workers --n_samples=10000 > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
+#python code/test.py --cnet_name=unet6sft --batch_size=$batch_size --num_workers=$num_workers --n_samples=10000 > output/salida_${SERVER}_test_${BASHPID}.txt 2>&1
 
 #################################################################################################
 
-
-<<COMMENT
 mnet_name_array=(mobilenetv3s)
 cnet_name_array=(unet6sft unet6)
 pretraining_epochs_array=(1)
-sigmaRui_sq_array=(0.05 0.001 0.1 0.2)
-theta_val_array=(1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.0)
+sigmaRui_sq_array=(0.05)
+#theta_val_array=(1.0 0.9 0.8 0.7 0.6 0.5 0.4 0.3 0.2 0.1 0.0)
+theta_val_array=(0.6 0.5 0.4 0.3 0.2 0.1 0.0)
+
 
 for cnet_name in "${cnet_name_array[@]}"
 do
@@ -38,12 +38,11 @@ do
             do
                 for theta in "${theta_val_array[@]}"
                 do
-                    #python code/train.py --val_type=GT --sigmaRui_sq=$sigmaRui_sq --batch_size=$batch_size --num_workers=$num_workers --pretraining_epochs=$pe --theta_val=$theta --cnet_name=$cnet_name --mnet_name=$mnet_name > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
-                    python code/train.py --val_type=normal --sigmaRui_sq=$sigmaRui_sq --batch_size=$batch_size --num_workers=$num_workers --pretraining_epochs=$pe --theta_val=$theta --cnet_name=$cnet_name --mnet_name=$mnet_name > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
+                    python code/train.py --val_type=GT --sigmaRui_sq=$sigmaRui_sq --batch_size=$batch_size --num_workers=$num_workers --pretraining_epochs=$pe --theta_val=$theta --cnet_name=$cnet_name --mnet_name=$mnet_name > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
+                    #python code/train.py --val_type=normal --sigmaRui_sq=$sigmaRui_sq --batch_size=$batch_size --num_workers=$num_workers --pretraining_epochs=$pe --theta_val=$theta --cnet_name=$cnet_name --mnet_name=$mnet_name > output/salida_${SERVER}_train_${BASHPID}.txt 2>&1
                     python code/test.py --sigmaRui_sq=$sigmaRui_sq --batch_size=$batch_size --num_workers=$num_workers --pretraining_epochs=$pe --theta_val=$theta --cnet_name=$cnet_name --mnet_name=$mnet_name > output/salida_${SERVER}_test_${BASHPID}.txt 2>&1
                 done
             done
         done
     done
 done
-COMMENT
--- code/train.py
+++ code/train.py
@@ -31,6 +31,7 @@ SAVE_MODEL_PATH = os.path.join(args.save_model_dir, f"{SAVE_MODEL_NAME}/")
 HISTORY_PATH = os.path.join(args.save_history_dir, f"{SAVE_MODEL_NAME}.csv")
 ES_METRIC="val_loss"
 TRAIN_CENTERS =  [0,2,4]
+VAL_CENTERS = [1,3]
 
 if not os.path.exists(args.save_history_dir):
     os.makedirs(args.save_history_dir)
@@ -38,20 +39,19 @@ if not os.path.exists(args.save_history_dir):
 if not os.path.exists(SAVE_MODEL_PATH):
     os.makedirs(SAVE_MODEL_PATH)
 
-cam_dataset = CamelyonDataset(args.camelyon_data_path, TRAIN_CENTERS, patch_size=args.patch_size, n_samples=args.n_samples)
+cam_train_dataset = CamelyonDataset(args.camelyon_data_path, TRAIN_CENTERS, patch_size=args.patch_size, n_samples=args.n_samples)
 if args.val_type == "GT":
     print("Using WSSB dataset for validation")
     ES_METRIC = "val_mse_gt"
-    train_dataset = cam_dataset
+    train_dataset = cam_train_dataset
     val_dataset = WSSBDatasetTest(args.wssb_data_path, organ_list=['Lung', 'Breast', 'Colon'])
     val_batch_size = 1
 else:
     print("Using Camelyon dataset for validation")
     ES_METRIC = "val_mse_rec"
-    len_ds = len(cam_dataset)
-    len_val = int(args.val_prop * len_ds)
-    len_train = len_ds - len_val
-    train_dataset, val_dataset = torch.utils.data.random_split(cam_dataset, [len_train, len_val], generator=torch.Generator().manual_seed(42))
+    train_dataset = cam_train_dataset
+    n_samples_val = int(args.val_prop * len(cam_train_dataset))
+    val_dataset = CamelyonDataset(args.camelyon_data_path, VAL_CENTERS, patch_size=args.patch_size, n_samples=args.n_samples)
     val_batch_size = args.batch_size
 
 train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers)
