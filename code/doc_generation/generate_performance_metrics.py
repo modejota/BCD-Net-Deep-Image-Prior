5522195390b98ea077e283c05e9d902238018006
José Alberto Gómez García <modej@correo.ugr.es>
2023-09-18 12:22:27 +0200

    New method to generate better graphs + minor improvements + loss metric

--- code/doc_generation/generate_performance_metrics.py
+++ code/doc_generation/generate_performance_metrics.py
@@ -81,10 +81,10 @@ def group_csv_files_by_approach(csv_filepaths):
     
     return approaches
 
-def generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results/',
-                                outdir='/home/modejota/Deep_Var_BCD/results/graphs/',
+def generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/',
+                                outdir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/graphs/',
                                 training_type='per_image_training',
-                                metrics_to_use=["psnr", "mse", "ssim"]):
+                                metrics_to_use=["psnr", "mse", "ssim", "loss"]):
     """Generate one graph for each approach and metric given a directory containing the CSV files with the metrics for a set of images. 
        Meant to be used for individual image training, if done for a big dataset graph could be too big to be useful
     Args:
@@ -104,29 +104,29 @@ def generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results/',
 
     method_groups = group_csv_files_by_approach(csv_files)
 
+    contador = 0
     for method_name in method_groups:
         print(f'Generating graphs for approach: {method_name}')
         csv_files = method_groups[method_name]
-        organ_info = []
-        model_name = []
+        filenames = []
         data_lists = {metric: [] for metric in metrics_to_use}
 
         csv_files.sort()
         for filename in csv_files:
             filepath = os.path.join(indir, filename)
-            model_name_, organ_info_ = get_model_and_organs_info(filepath, training_type=training_type)
-            organ_info.append(organ_info_)
-            model_name.append(model_name_)
+            contador += 1
+            filenames.append(os.path.basename(filename)[:-4])
 
             df = pd.read_csv(filepath)
             for metric in metrics_to_use:
-                columns = [f'{metric}_gt', f'{metric}_gt_e', f'{metric}_gt_h']
+                columns = [f'{metric}_gt', f'{metric}_gt_e', f'{metric}_gt_h'] if metric != 'loss' else [f'{metric}']
                 for column in columns:
                     if column in df.columns:
                         data = df[column]
                         data_lists[metric].append(data)
 
-        num_rows = (len(organ_info) + 2) // 3
+        num_rows = (contador + 2) // 3
+        contador = 0
 
         for metric in metrics_to_use:
             fig, ax = plt.subplots(num_rows, 3, figsize=(10*num_rows, 5 * num_rows))
@@ -135,17 +135,22 @@ def generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results/',
             for row in range(num_rows):
                 for col in range(3):
                     idx = row * 3 + col
-                    if idx < len(organ_info):
-                        organ = organ_info[idx]
+                    if idx < len(filenames):
+                        organ = filenames[idx]
                         ax[row, col].set_title('Organ: ' + organ)
                         ax[row, col].set_xlabel('Iterations')
                         ax[row, col].set_ylabel('Value')
 
-                        data_idx = idx * 3
-                        ax[row, col].plot(data_lists[metric][data_idx + 1], label=f'{metric}_gt_e', color='steelblue')
-                        ax[row, col].plot(data_lists[metric][data_idx + 2], label=f'{metric}_gt_h', color='orange')
-                        ax[row, col].plot(data_lists[metric][data_idx], label=f'{metric}_gt', color='green')
+                        if metric != "loss":
+                            data_idx = idx * 3
+                            ax[row, col].plot(data_lists[metric][data_idx + 1], label=f'{metric}_gt_e', color='steelblue')
+                            ax[row, col].plot(data_lists[metric][data_idx + 2], label=f'{metric}_gt_h', color='orange')
+                            ax[row, col].plot(data_lists[metric][data_idx], label=f'{metric}_gt', color='green')
 
+                            if metric == 'ssim':
+                                ax[row, col].set_ylim(0, None)
+                        else:
+                            ax[row, col].plot(data_lists[metric][idx], label=f'{metric}', color='red')
 
                         ax[row, col].legend()
 
@@ -157,7 +162,8 @@ def generate_graphs_by_image(organ: str, id: int,
                              outdir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/graphs/',
                              training_type='per_image_training',
                              metrics_to_use=["psnr", "mse", "ssim"]):
-    """Generate one graph for each image and metric given a directory containing the CSV files with the metrics for a set of approaches
+    """Generate one graph for each image and metric given a directory containing the CSV files with the metrics for a set of approaches.
+    This method is meant to be used when the number of graphs per approach is unknown. It will generate a square grid of graphs.
     Args:
         organ (str): The name of the organ
         id (str): The id of the image
@@ -194,20 +200,109 @@ def generate_graphs_by_image(organ: str, id: int,
             model, _ = get_model_and_organs_info(csv_file, training_type='per_image_training')
 
             df = pd.read_csv(csv_file)
-            values_gt = df[f'{metric}_gt']
-            values_gt_e = df[f'{metric}_gt_e']
-            values_gt_h = df[f'{metric}_gt_h']
+            if metric != "loss":
+                values_gt = df[f'{metric}_gt']
+                values_gt_e = df[f'{metric}_gt_e']
+                values_gt_h = df[f'{metric}_gt_h']
 
-            row_idx = i // num_columns
-            col_idx = i % num_columns
+                row_idx = i // num_columns
+                col_idx = i % num_columns
 
-            axs[row_idx, col_idx].plot(values_gt_e, label=f'{metric}_gt_e')
-            axs[row_idx, col_idx].plot(values_gt_h, label=f'{metric}_gt_h')
-            axs[row_idx, col_idx].plot(values_gt, label=f'{metric}_gt')
-            axs[row_idx, col_idx].set_title(f'Approach: {model}')
-            axs[row_idx, col_idx].set_xlabel('Iterations')
-            axs[row_idx, col_idx].set_ylabel(f'{metric.upper()}')
-            axs[row_idx, col_idx].legend()
+                axs[row_idx, col_idx].plot(values_gt_e, label=f'{metric}_gt_e')
+                axs[row_idx, col_idx].plot(values_gt_h, label=f'{metric}_gt_h')
+                axs[row_idx, col_idx].plot(values_gt, label=f'{metric}_gt')
+                axs[row_idx, col_idx].set_title(f'Approach: {model}')
+                axs[row_idx, col_idx].set_xlabel('Iterations')
+                axs[row_idx, col_idx].set_ylabel(f'{metric.upper()}')
+                if metric == 'ssim':
+                    axs[row_idx, col_idx].set_ylim(0, None)
+                axs[row_idx, col_idx].legend()
+            else:
+                values_loss = df[f'{metric}']
+                row_idx = i // num_columns
+                col_idx = i % num_columns
+
+                if (row_idx == num_rows - 1):   # CNET only has 2 graphs, centered in the last row
+                    col_idx += 1
+                
+                axs[row_idx, col_idx].plot(values_loss, label=f'{metric}')
+                axs[row_idx, col_idx].set_title(f'Approach: {model}')
+                axs[row_idx, col_idx].set_xlabel('Iterations')
+                axs[row_idx, col_idx].set_ylabel(f'{metric.upper()}')
+                axs[row_idx, col_idx].legend()
+
+        fig.suptitle(f'Organ: {organ} - Image: {id}')
+        plt.savefig(os.path.join(outdir, f'{organ}_{id}_{metric.upper()}.png'))
+        plt.close()
+
+def generate_graphs_by_image_v2(organ: str, id: int,
+                                indir ='/home/modejota/Deep_Var_BCD/results_reduced_datasets/',
+                                outdir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/graphs/',
+                                training_type='per_image_training',
+                                metrics_to_use=["psnr", "mse", "ssim", "loss"]):
+    """Generate one graph for each image and metric given a directory containing the CSV files with the metrics for a set of approaches.
+    This method should be used when the it's known that there will be 4 graphs per approach (image, image_weights, noise, noise_weights) for a set number of approaches + 2 graphs of CNET_E2 (image, noise).
+    Args:
+        organ (str): The name of the organ
+        id (str): The id of the image
+        outdir (str): The path to the directory where the graphs will be saved
+    """
+    organ = organ.lower().capitalize()
+    if id < 0:
+        return
+    filename = f'{organ}_{id}/metrics.csv' if indir == '/home/modejota/Deep_Var_BCD/results_reduced_datasets/' else f'{organ}_{id}.csv'
+
+    outdir += os.sep if outdir[-1] != os.sep else ''
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    csv_files = get_certain_csv_filepaths(filename, directory_path=indir, training_type=training_type)
+    csv_files.sort()
+    num_files = len(csv_files)
+    num_rows = (num_files - 2) // 4 + 1
+    num_columns = 4
+
+    for metric in metrics_to_use:
+        fig, axs = plt.subplots(num_rows, num_columns, figsize=(10*num_columns, 5 * num_rows))
+        axs[num_rows-1, 0].set_visible(False)
+        axs[num_rows-1, num_columns-1].set_visible(False)
+        for i, csv_file in enumerate(csv_files):
+            model, _ = get_model_and_organs_info(csv_file, training_type='per_image_training')
+
+            df = pd.read_csv(csv_file)
+            if metric != "loss":
+                values_gt = df[f'{metric}_gt']
+                values_gt_e = df[f'{metric}_gt_e']
+                values_gt_h = df[f'{metric}_gt_h']
+                
+                row_idx = i // num_columns
+                col_idx = i % num_columns
+
+                if (row_idx == num_rows - 1):   # CNET only has 2 graphs, centered in the last row
+                    col_idx += 1
+
+                axs[row_idx, col_idx].plot(values_gt_e, label=f'{metric}_gt_e')
+                axs[row_idx, col_idx].plot(values_gt_h, label=f'{metric}_gt_h')
+                axs[row_idx, col_idx].plot(values_gt, label=f'{metric}_gt')
+                axs[row_idx, col_idx].set_title(f'Approach: {model}')
+                axs[row_idx, col_idx].set_xlabel('Iterations')
+                axs[row_idx, col_idx].set_ylabel(f'{metric.upper()}')
+                if metric == 'ssim':
+                    axs[row_idx, col_idx].set_ylim(0, None)
+                axs[row_idx, col_idx].legend()
+            else:
+                values_loss = df[f'{metric}']
+                row_idx = i // num_columns
+                col_idx = i % num_columns
+
+                if (row_idx == num_rows - 1):   # CNET only has 2 graphs, centered in the last row
+                    col_idx += 1
+                
+                axs[row_idx, col_idx].plot(values_loss, label=f'{metric}')
+                axs[row_idx, col_idx].set_title(f'Approach: {model}')
+                axs[row_idx, col_idx].set_xlabel('Iterations')
+                axs[row_idx, col_idx].set_ylabel(f'{metric.upper()}')
+                axs[row_idx, col_idx].legend()
 
         fig.suptitle(f'Organ: {organ} - Image: {id}')
         plt.savefig(os.path.join(outdir, f'{organ}_{id}_{metric.upper()}.png'))
@@ -284,20 +379,22 @@ def generate_metric_report_all_methods(indir='/home/modejota/Deep_Var_BCD/result
     for ruta_completa in directorios:
         generate_metrics_report(os.path.basename(ruta_completa), indir, outdir, use_inner_directory=False)
 
-EXECUTE_SAMPLES = True
+EXECUTE_SAMPLES = False
 if __name__ == "__main__":
 
     if EXECUTE_SAMPLES:
-        # print("Generating graphs for all approaches and selected images.")
-        # generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/graphs/', training_type='per_image_training')
+        print("\nGenerating graphs for all approaches and selected images.")
+        generate_graphs_by_approach(indir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_reduced_datasets/graphs/', training_type='batch_training')
         
-        print("Generating graphs for selected images.")
-        generate_graphs_by_image(organ='Colon', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
-        generate_graphs_by_image(organ='Colon', id=6, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
-        generate_graphs_by_image(organ='Lung', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
-        generate_graphs_by_image(organ='Lung', id=48, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
-        generate_graphs_by_image(organ='Breast', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
-        generate_graphs_by_image(organ='Breast', id=48, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
         
+        print("\nGenerating graphs for selected images.")
+        generate_graphs_by_image_v2(organ='Colon', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        generate_graphs_by_image_v2(organ='Colon', id=6, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        generate_graphs_by_image_v2(organ='Lung', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        generate_graphs_by_image_v2(organ='Lung', id=48, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        generate_graphs_by_image_v2(organ='Breast', id=0, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        generate_graphs_by_image_v2(organ='Breast', id=48, indir='/home/modejota/Deep_Var_BCD/results_full_datasets/', outdir='/home/modejota/Deep_Var_BCD/results_full_datasets/graphs/', training_type='batch_training')
+        
+
         print("\nGenerating metrics' report for all methods.")
         generate_metric_report_all_methods()
